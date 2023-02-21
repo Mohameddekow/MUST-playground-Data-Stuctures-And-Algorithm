7e4d060063f1010bea0eb0d30afe573094538483
mohameddekow <mohameddekow77@gmail.com>
2023-02-21 23:47:39 +0300

    added Valid anagram  solution(python)

---
+++ Arays and Hashing/242_Valid Anagram.py
@@ -0,0 +1,41 @@
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+
+class ValidAnagramSolution: 
+    def solution(self, s: str, t:str) -> bool:
+        sDictionary = {}
+        tDictionary = {}
+
+        # create a dictionary for string s{ key = letter: value = it's counts}
+        for i in s:
+            sDictionary[i] = sDictionary.get(i, 0) + 1
+
+
+        # create a dictionary for string t{ key = letter: value = it's counts}
+        for j in t:
+            tDictionary[j] = tDictionary.get(t, 0)  + 1
+
+
+        if sDictionary == tDictionary:
+            print(True)
+            return True
+
+        print(False)
+        return False
+        # return sDictionary == tDictionary
+
+
+anagamClass = ValidAnagramSolution()
+
+anagram = anagamClass.solution("anagram", "nagaram")
